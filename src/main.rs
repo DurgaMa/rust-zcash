ed4cca17a17d4d885edd0c2d2233a78ecf806e38
David Partouche <DurgaMa@users.noreply.github.com>
2017-02-24 20:51:57 +0800

    Add the version option to the command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    if std::env::args().any(|a| a == "--version" || a == "-v") {
+        println!("{}\n", version_message());
+        return;
+    }
+}
+
+fn version_message() -> String {
+    format!("Zcash RPC client version v{}", env!("CARGO_PKG_VERSION"))
 }
